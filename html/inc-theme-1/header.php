e5ac73927bcba7e29679baf49ba1dbe217b908a5
rnnch29 <115811785+rnnch29@users.noreply.github.com>
2024-02-07 13:21:42 +0700

    html

---
+++ html/inc-theme-1/header.php
@@ -0,0 +1,51 @@
+<header>
+    <div class="nav-bar">
+        <div class="row align-items-center">
+            <div class="col">
+
+                <a class="link" href="#">
+                    <div class="row align-items-center">
+                        <div class="col-auto">
+                            <img src="../assets/img/logo.png" alt="">
+                        </div>
+                        <div class="col-auto">
+                            <div class="nav-title" >
+                                NEXT GEN
+                            </div>
+                        </div>
+                    </div>
+                </a>
+
+            </div>
+
+            <!-- <div class="col-auto">
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        TH
+                    </button>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                        <a class="dropdown-item" href="#">TH</a>
+                        <a class="dropdown-item" href="#">EN</a>
+                    </div>
+                </div>
+            </div> -->
+
+            <div class="dropdown">
+                <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <img src="../assets/img/icon/sss.png" alt="">
+                </a>
+
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <a class="dropdown-item" href="#">
+                        <img src="../assets/img/icon/eee.png" alt="">
+                    </a>
+                    <a class="dropdown-item" href="#">
+                        <img src="../assets/img/icon/sss.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</header>
